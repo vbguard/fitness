93e88cf364b623683c95199d99eb706a3ba7f2c1
Viktor Bulvarenko <viktor@bulvarenko.pw>
2018-09-22 14:43:49 +0300

    add alias to folder in webpack config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-const store = createStore();
+// const store = createStore();
 
-store.dispatch('fo');
+// store.dispatch('fo');
